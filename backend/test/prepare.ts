997f22542aaa8f89085d13285efffd66ecb50514
Leonardo Panseri <leonardo.panseri@gmail.com>
2022-11-17 00:44:49 +0100

    chore: Remove redundant module

--- backend/test/prepare.ts
+++ backend/test/prepare.ts
@@ -1,4 +1,3 @@
-import prepare from "mocha-prepare";
 import { start, stop } from "mongo-unit";
 import { config } from "dotenv";
 import { join } from "path";
@@ -7,14 +6,13 @@ import { join } from "path";
 config({ path: join(__dirname, ".test.env") });
 
 // Register callbacks that will be executed when loading/unloading the entire testing environment
-prepare(done => {
+export const mochaGlobalSetup = async () => {
     // Start the mongo-unit DB instance.
     // mongo-unit will run a MongoDB instance in memory to simplify and speed up testing
-    start().then(dbString => {
-        process.env.TEST_DB_STRING = dbString;
-        done();
-    });
-}, done => {
+    process.env.TEST_DB_STRING = await start();
+};
+
+export const mochaGlobalTeardown = async () => {
     // Stop the mongo-unit DB instance
-    stop().then(() => done());
-});
+    await stop();
+};
