14aab2eebfd7f64ecbbc466b44cd361a06f1396a
MicheleBenedetti <45004856+Michele-x98@users.noreply.github.com>
2022-11-17 16:21:33 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,27 +5,23 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#0f172a',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'grey': '#8B858F',
-      'bermuda': '#78dcca',
-      'black': '#232323',
-      'grey2': '#363636',
-      'blue2': '#1E85FF',
-      'blue3': '#004F72',
-    },
-
-
-
     extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#0f172a',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'grey': '#8B858F',
+        'bermuda': '#78dcca',
+        'black': '#232323',
+        'grey2': '#363636',
+        'blue2': '#1E85FF',
+        'blue3': '#004F72',
+      },
     },
   },
   plugins: [],
