91bcb181920865c299e1308677fdc0cd8f8fa466
Ivan Radonjic <118177516+chaser-22@users.noreply.github.com>
2022-11-17 16:26:16 +0100

    Colors update & comments deletion [develop]

--- frontend/src/views/PersonalPage.vue
+++ frontend/src/views/PersonalPage.vue
@@ -9,9 +9,9 @@
     <h1 class="text-5xl text-grey text-center py-20">Personal Page</h1>
 
     <div
-      class="container mx-auto grid grid-cols-1 bg-grey2 rounded-md px-5 py-5 border-2 border-blue2 gap-4 overflow-y-scroll h-96 overscroll-auto"
+      class="container mx-auto grid grid-cols-1 bg-darkGrey rounded-md px-5 py-5 border-2 border-lightBlue gap-4 overflow-y-scroll h-96 overscroll-auto"
     >
-      <div class="text-white px-4 bg-blue3 w-full py-5">
+      <div class="text-white px-4 bg-darkBlue w-full py-5">
         RULE 1
         <button class="float-right">
           <svg
@@ -30,7 +30,7 @@
           </svg>
         </button>
       </div>
-      <div class="text-white px-4 bg-blue3 w-full py-5">
+      <div class="text-white px-4 bg-darkBlue w-full py-5">
         RULE 2
         <button class="float-right">
           <svg
@@ -49,7 +49,7 @@
           </svg>
         </button>
       </div>
-      <div class="text-white px-4 bg-blue3 w-full py-5">
+      <div class="text-white px-4 bg-darkBlue w-full py-5">
         RULE 3
         <button class="float-right">
           <svg
@@ -68,7 +68,7 @@
           </svg>
         </button>
       </div>
-      <div class="text-white px-4 bg-blue3 w-full py-5">
+      <div class="text-white px-4 bg-darkBlue w-full py-5">
         RULE 4
         <button class="float-right">
           <svg
@@ -106,7 +106,7 @@
           </svg>
         </button>
       </div>
-      <div class="text-white px-4 bg-blue3 w-full py-5">
+      <div class="text-white px-4 bg-darkBlue w-full py-5">
         RULE 6
         <button class="float-right">
           <svg
@@ -127,114 +127,15 @@
       </div>
     </div>
 
-    <!-- <div class="fixed text-left bottom-72 left-96">
-      <button
-        class="bg-blue3 hover:bg-blue-700 text-white font-bold py-2 px-12 rounded-md"
-      >
-        Add new rules
-      </button>
-    </div>
-
-    <div class="fixed  text-right bottom-72 right-96">
-      <button
-        class="bg-blue3 hover:bg-blue-700 text-white font-bold py-2 px-12 rounded-md"
-      >
-        See authorized services
-      </button>
-    </div> -->
-
     <div class="flex flex-row justify-center py-10 space-x-32">
-      <button type="button" class="px-5 py-3 bg-blue3 text-white">
+      <button type="button" class="px-5 py-3 bg-darkBlue text-white">
         Add new rules
       </button>
-      <button type="button" class="px-5 py-3 bg-blue3 text-white">
+      <button type="button" class="px-5 py-3 bg-darkBlue text-white">
         See authorized devices
       </button>
     </div>
 
-    <!-- <header class="flex justify-between py-4 px-8 text-white">
-
-
-        <a href="/" class="text-lg font-bold"> Logo </a>
-
-        <ul class="hidden md:flex flex-row gap-4 items-center align-middle">
-                <li><a href="#" class="bg-bubble-gum py-2 px-2 rounded-md">Home</a> </li>
-                <li><a href="#" class="">About</a> </li>
-                <li><a href="#" class="">Contact</a> </li>
-        </ul>
-
-        <button class="md:hidden">
-
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
-              </svg>
-            </button>
-    
-    </header>
-    
-    <div class="container mx-auto">
-
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-
-
-            <div class="bg-white rounded-md px-5 py-5 border-2 border-midnight">
-            <h2 class="tracking-widest text-xs mb-1">CATEGORY</h2>
-            <h1 class="title-font text-lg text-silver mb-3">TailwindCSS</h1>
-            <p class="leading-relaxed text-sm mb-3">
-                aafafafafa
-                fafafafafafaf
-                fafafafafafafaffaf
-                fafafafafafafaffaffafa
-                fafafafafafafaffaffafa
-            </p>
-            <button class="text-sm bg-midnight text-white rounded-md py-2 px-2">Click me</button>
-        </div>
-
-
-        <div class="bg-white rounded-md px-5 py-5 border-2 border-midnight">
-            <h2 class="tracking-widest text-xs mb-1">CATEGORY</h2>
-            <h1 class="title-font text-lg text-silver mb-3">TailwindCSS</h1>
-            <p class="leading-relaxed text-sm mb-3">aafafafafa
-                fafafafafafaf
-                fafafafafafafaffaf
-                fafafafafafafaffaffafa
-                fafafafafafafaffaffafa
-            </p>
-            <button class="text-sm bg-midnight text-white rounded-md py-2 px-2">Click me</button>
-        </div>
-        
-
-        <div class="bg-white rounded-md px-5 py-5 border-2 border-midnight">
-            <h2 class="tracking-widest text-xs mb-1">CATEGORY</h2>
-            <h1 class="title-font text-lg text-silver mb-3">TailwindCSS</h1>
-            <p class="leading-relaxed text-sm mb-3">aafafafafa
-                fafafafafafaf
-                fafafafafafafaffaf
-                fafafafafafafaffaffafa
-                fafafafafafafaffaffafa
-            </p>
-            <button class="text-sm bg-midnight text-white rounded-md py-2 px-2">Click me</button>
-        </div>
-
-
-        <div class="bg-white rounded-md px-5 py-5 border-2 border-midnight">
-            <h2 class="tracking-widest text-xs mb-1">CATEGORY</h2>
-            <h1 class="title-font text-lg text-silver mb-3">TailwindCSS</h1>
-            <p class="leading-relaxed text-sm mb-3">aafafafafa
-                fafafafafafaf
-                fafafafafafafaffaf
-                fafafafafafafaffaffafa
-                fafafafafafafaffaffafa
-            </p>
-            <button class="text-sm bg-midnight text-white rounded-md py-2 px-2 hover:bg-bubble-gum">Click me</button>
-        </div>
-
-
-
-        
-
-
-    </div> -->
   </div>
 </template>
 
