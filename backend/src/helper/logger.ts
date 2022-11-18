7fdea7ff39f61fa53b9be08099bd296f83e258d3
Leonardo Panseri <leonardo.panseri@gmail.com>
2022-11-18 18:59:30 +0100

    chore [PRIV-36]: Disable logger in testing environment

--- backend/src/helper/logger.ts
+++ backend/src/helper/logger.ts
@@ -1,7 +1,16 @@
 import { colorConsole } from "tracer";
+import env from "./env";
+
+let level = "fatal";
+
+if (env.DEV)
+    level = "log";
+
+if (env.PROD)
+    level = "info";
 
 const logger = colorConsole({
-    level: "log",
+    level: level,
     dateformat: "dd-m hh:mm"
 });
 export default logger;
