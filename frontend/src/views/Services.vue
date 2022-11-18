19f11621880c9429c19ced8407338797e972727d
Ivan Radonjic <irc21001@student.mdu.se>
2022-11-18 19:04:46 +0100

    Updated Services [develop]

--- frontend/src/views/Services.vue
+++ frontend/src/views/Services.vue
@@ -30,30 +30,11 @@
           placeholder="Search"
           >
       </div>
-      <!-- <button
-        type="submit"
-        class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        <svg
-          class="w-5 h-5"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          ></path>
-        </svg>
-        <span class="sr-only">Search</span>
-      </button> -->
+      
     </form>
 
     <div
-      class="container mx-auto grid grid-cols-1 bg-gray-800 rounded-md px-5 py-5 border-2 border-blue2 gap-4 overflow-y-scroll h-96 overscroll-auto"
+      class="container mx-auto grid grid-cols-1 bg-gray-800 rounded-md px-5 py-5 border-2 border-blue-600 gap-4 overflow-y-scroll h-96 overscroll-auto"
     >
       <div class="text-white px-4 bg-blue-600 w-full py-5">
         SERVICE 1...
