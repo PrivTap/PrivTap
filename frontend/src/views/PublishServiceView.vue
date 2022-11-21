b1a7ec3e339eb3bb32f191b23d60c22211018bc3
MicheleBenedetti <45004856+Michele-x98@users.noreply.github.com>
2022-11-21 20:22:43 +0100

    Feat: [develop] Adds logic on PublishServicePage
    Fix few aspects aslo

--- frontend/src/views/PublishServiceView.vue
+++ frontend/src/views/PublishServiceView.vue
@@ -1,57 +1,171 @@
 <template>
-
-<div>
-    <h1 class="text-5xl text-blue-100 font-medium text-center py-20">Publish Service</h1>
-
-
-
-
-    
-<form>
-
-    <div class="mb-6 mx-auto w-96">
-      <label for="text" class="block mb-2 text-sm font-semibold text-white dark:text-gray-300">Name</label>
-      <input type="text" id="text" class="bg-gray-50 border border-gray-600 text-white text-md rounded-lg block w-full p-2.5 dark:bg-gray-700" placeholder="...">  
-      <!-- Required as a command is missing in above input -->
-    </div>
-
-
-    <div class="mb-6 mx-auto w-96">
-        <label for="text" class="block mb-2 text-sm font-semibold text-white dark:text-gray-300">Description</label>
-        <input type="text" id="text" class="bg-gray-50 border border-gray-600 text-white text-md rounded-lg block w-full p-2.5 dark:bg-gray-700" placeholder="...">
+  <div>
+    <h1 class="text-5xl text-blue-100 font-medium text-center py-20">
+      Publish Service
+    </h1>
+    <form @submit.prevent="onSubmitted">
+      <div class="mb-6 mx-auto w-96">
+        <label
+          for="text"
+          class="block mb-2 text-sm font-semibold text-white dark:text-gray-300"
+          >Name</label
+        >
+        <input
+          type="text"
+          id="text"
+          class="border-2 border-gray-600 text-white text-md rounded-lg block w-full p-2.5 dark:bg-gray-700 focus:outline-none focus:border-blue-500"
+          placeholder="..."
+          v-model="newService.name"
+          required="true"
+        />
+        <div v-show="!newService.name.length" class="animate-fade-in mt-2">
+          <span class="font-medium text-sm ml-3 text-red-200">
+            Please, provide a service name</span
+          >
+        </div>
       </div>
-
 
       <div class="mb-6 mx-auto w-96">
-        <label for="text" class="block mb-2 text-sm font-semibold text-white dark:text-gray-300">Required data</label>
-        <input type="text" id="text" class="bg-gray-50 border border-gray-600 text-white text-md rounded-lg block w-full p-2.5 dark:bg-gray-700" placeholder="...">
+        <label
+          for="text"
+          class="block mb-2 text-sm font-semibold text-white dark:text-gray-300"
+          required="true"
+          >Description</label
+        >
+        <input
+          type="text"
+          id="text"
+          class="border-2 border-gray-600 text-white text-md rounded-lg block w-full p-2.5 dark:bg-gray-700 focus:outline-none focus:border-blue-500"
+          placeholder="..."
+          v-model="newService.description"
+          required="true"
+        />
+        <div
+          v-show="!newService.description.length"
+          class="animate-fade-in mt-2"
+        >
+          <span class="font-medium text-sm ml-3 text-red-200">
+            Please, provide a service description</span
+          >
+        </div>
       </div>
-
 
       <div class="mb-6 mx-auto w-96">
-        <label for="text" class="block mb-2 text-sm font-semibold text-white dark:text-gray-300">Endpoint link</label>
-        <input type="text" id="text" class="bg-gray-50 border border-gray-600 text-white text-md rounded-lg block w-full p-2.5 dark:bg-gray-700" placeholder="...">
+        <label
+          for="text"
+          class="block mb-2 text-sm font-semibold text-white dark:text-gray-300"
+          >Endpoint Url</label
+        >
+        <input
+          type="text"
+          id="text"
+          class="border-2 border-gray-600 text-white text-md rounded-lg block w-full p-2.5 dark:bg-gray-700 focus:outline-none focus:border-blue-500"
+          placeholder="http://example.com:8080"
+          v-model="newService.authUrl"
+          required="true"
+        />
+        <div v-show="!isValidUrl" class="animate-fade-in mt-2">
+          <span
+            :class="{
+              'text-green-400': isValidUrl,
+              'text-red-200': !isValidUrl,
+            }"
+            class="font-medium text-sm ml-3"
+            v-text="isValidUrl ? '' : 'Insert a valid url'"
+          ></span>
+        </div>
       </div>
 
-</form>
-  
+      <div class="mb-6 mx-auto w-96">
+        <label
+          type="url"
+          class="block mb-2 text-sm font-semibold text-white dark:text-gray-300"
+          >Client ID</label
+        >
+        <input
+          type="text"
+          id="text"
+          class="border-2 border-gray-600 text-white text-md rounded-lg block w-full p-2.5 dark:bg-gray-700 focus:outline-none focus:border-blue-500"
+          placeholder="..."
+          v-model="newService.clientID"
+          required="true"
+        />
+        <div v-show="!newService.clientID.length" class="animate-fade-in mt-2">
+          <span class="font-medium text-sm ml-3 text-red-200">
+            Plese, provice a client ID</span
+          >
+        </div>
+      </div>
 
-<div class="flex flex-row justify-center py-10 space-x-32">
-    <button type="button" class="px-16 py-3 bg-blue-800 rounded-md text-white font-semibold">
-      Create API endpoint
-    </button>
+      <div class="mb-6 mx-auto w-96">
+        <label
+          type="url"
+          class="block mb-2 text-sm font-semibold text-white dark:text-gray-300"
+          >Client Secret</label
+        >
+        <input
+          type="text"
+          id="text"
+          class="border-2 border-gray-600 text-white text-md rounded-lg block w-full p-2.5 dark:bg-gray-700 focus:outline-none focus:border-blue-500"
+          placeholder="..."
+          v-model="newService.clientSecret"
+          required="true"
+        />
+        <div
+          v-show="!newService.clientSecret.length"
+          class="animate-fade-in mt-2"
+        >
+          <span class="font-medium text-sm ml-3 text-red-200">
+            Please, provide a client secret</span
+          >
+        </div>
+      </div>
+      <div class="flex flex-row justify-center py-10 space-x-32">
+        <button
+          type="button"
+          class="px-16 py-3 bg-blue-800 rounded-md text-white font-semibold"
+        >
+          Create API endpoint
+        </button>
+      </div>
+    </form>
   </div>
-
-</div>
-</template>>
-
+</template>
+>
 
 <script setup lang="ts">
+import { useOspServiceStore } from "@/stores/osp_service_store";
+import { ref, watch } from "vue";
+const newService = ref({
+  name: "",
+  description: "",
+  authUrl: "",
+  clientID: "",
+  clientSecret: "",
+});
 
-/// Scripts
+const isValidUrl = ref<boolean>(true);
 
+watch(newService.value, (_, newValue) => {
+  isValidUrl.value = checkUrl(newValue.authUrl);
+});
+
+/// function that check if a string is a valid url
+function checkUrl(url: string): boolean {
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
+    url
+  );
+}
+
+const manageServiceStore = useOspServiceStore();
+const onSubmitted = () =>
+  manageServiceStore.addService(
+    newService.value.name,
+    newService.value.description,
+    newService.value.authUrl,
+    newService.value.clientID,
+    newService.value.clientSecret
+  );
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
