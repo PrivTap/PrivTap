7089539014d41c98fab3cca4397330b419f283ba
MicheleBenedetti <45004856+Michele-x98@users.noreply.github.com>
2022-11-23 16:07:42 +0100

    test: [frontend/service-test] adds service store tests

--- frontend/src/stores/osp_service_store.ts
+++ frontend/src/stores/osp_service_store.ts
@@ -4,6 +4,7 @@ import { ref, type Ref, reactive, getCurrentInstance, onMounted } from "vue";
 import ManageService from "@/services/manage_service";
 import { useToast } from "vue-toastification";
 import type { StandartRepsonse } from "@/model/response_model";
+import axiosCatch from "@/helpers/axios_catch";
 
 export default interface IOspServiceStoreState {
   services: Ref<Array<ServiceModel> | []>;
@@ -20,86 +21,120 @@ export default interface IOspServiceStoreState {
   updateService: (service: ServiceModel) => void;
 }
 
-export const useOspServiceStore = defineStore("osp_service_store", () => {
-  let services = ref<Array<ServiceModel> | []>([]);
-  const manageService = new ManageService();
-  const toast = useToast();
-
-  async function getServices() {
-    const res = await new ManageService().getAllServices();
-    if (res.status) {
-      setServices(res.data as ServiceModel[]);
-    }
-  }
-
-  if (getCurrentInstance()) {
-    console.log("manage service mounted");
-    onMounted(async () => {
-      const res = await new ManageService().getAllServices();
-      if (res.status) {
-        setServices(res.data as ServiceModel[]);
+export const useOspServiceStore = defineStore("ospServiceStore", {
+  state: () => ({
+    services: ref<Array<ServiceModel>>([]),
+    manageService: new ManageService(),
+  }),
+  actions: {
+    async getServices() {
+      try {
+        const response = await this.manageService.getAllServices();
+        if (response.status) {
+          this.services = response.data as Array<ServiceModel>;
+        }
+      } catch (error) {
+        axiosCatch(error);
       }
-    });
-  }
-
-
-  const setServices = (newServices: Array<ServiceModel>) => {
-    services.value = newServices;
-  };
-  const deleteService = async (serviceId: string) => {
-    const res = await manageService.deleteService(serviceId);
-    if (res.status) {
-      services.value = services.value.filter(
+    },
+    deleteService(serviceId: string) {
+      this.services = this.services.filter(
         (service) => service._id !== serviceId
       );
-      toast.success("Service deleted successfully");
-      return;
-    }
-    toast.error(res.message);
-    return;
-  };
-
-  async function addService(
-    name: string,
-    description: string,
-    authUrl: string,
-    clientID: string,
-    clientSecret: string
-  ) {
-    const res = await manageService.createService(
-      name,
-      description,
-      authUrl,
-      clientID,
-      clientSecret
-    );
-    if (res.status) {
-      const newService = res.data as ServiceModel;
-      services.value = [...services.value, newService];
-      if (res) {
-        useToast().success("Service added successfully");
-        return true;
-      }
-    }
-    useToast().error(res.message);
-    return false;
-  }
-
-  const updateService = (service: ServiceModel) => {
-    services.value = services.value.map((oldService) => {
-      if (oldService._id === service._id) {
-        return service;
-      }
-      return oldService;
-    });
-  };
-
-  return {
-    services,
-    setServices,
-    deleteService,
-    addService,
-    updateService,
-    getServices,
-  };
+    },
+    addService(newService: ServiceModel) {
+      this.services.push(newService);
+    },
+    updateService(updatedService: ServiceModel) {
+      this.services = this.services.map((oldService) => {
+        if (oldService._id === updatedService._id) {
+          return updatedService;
+        }
+        return oldService;
+      });
+    },
+  },
 });
+
+// export const useOspServiceStore = defineStore("osp_service_store", () => {
+//   let services = ref<Array<ServiceModel> | []>([]);
+//   const manageService = new ManageService();
+//   const toast = useToast();
+
+//   async function getServices() {
+//     const res = await new ManageService().getAllServices();
+//     if (res.status) {
+//       setServices(res.data as ServiceModel[]);
+//     }
+//   }
+
+//   if (getCurrentInstance()) {
+//     console.log("manage service mounted");
+//     onMounted(async () => {
+//       const res = await new ManageService().getAllServices();
+//       if (res.status) {
+//         setServices(res.data as ServiceModel[]);
+//       }
+//     });
+//   }
+
+//   const setServices = (newServices: Array<ServiceModel>) => {
+//     services.value = newServices;
+//   };
+//   const deleteService = async (serviceId: string) => {
+//     const res = await manageService.deleteService(serviceId);
+//     if (res.status) {
+//       services.value = services.value.filter(
+//         (service) => service._id !== serviceId
+//       );
+//       toast.success("Service deleted successfully");
+//       return;
+//     }
+//     toast.error(res.message);
+//     return;
+//   };
+
+//   async function addService(
+//     name: string,
+//     description: string,
+//     authUrl: string,
+//     clientID: string,
+//     clientSecret: string
+//   ) {
+//     const res = await manageService.createService(
+//       name,
+//       description,
+//       authUrl,
+//       clientID,
+//       clientSecret
+//     );
+//     if (res.status) {
+//       const newService = res.data as ServiceModel;
+//       services.value = [...services.value, newService];
+//       if (res) {
+//         useToast().success("Service added successfully");
+//         return true;
+//       }
+//     }
+//     useToast().error(res.message);
+//     return false;
+//   }
+
+//   const updateService = (service: ServiceModel) => {
+//     services.value = services.value.map((oldService) => {
+//       if (oldService._id === service._id) {
+//         return service;
+//       }
+//       return oldService;
+//     });
+//   };
+
+//   return {
+//     services,
+//     setServices,
+//     deleteService,
+//     addService,
+//     updateService,
+//     getServices,
+//   };
+// });
--- frontend/test/store_test/manage_service_store_test.ts
+++ frontend/test/store_test/manage_service_store_test.ts
@@ -1,39 +1,66 @@
-import { setActivePinia, createPinia } from "pinia";
-import IOspServiceStoreState, {
-  useOspServiceStore,
-} from "../../src/stores/osp_service_store";
+import { setActivePinia, createPinia, Store } from "pinia";
+import { useOspServiceStore } from "../../src/stores/osp_service_store";
 import ServiceModel from "../../src/model/service_model";
-import { describe, expect, it, beforeEach, beforeAll, test } from "vitest";
+import { describe, expect, it, beforeEach, afterEach } from "vitest";
 
 describe("Manage Service Store Test", () => {
-  let ospServiceStore;
-
+  let testServiceModel = new ServiceModel(
+    "id",
+    "name",
+    "description",
+    "creator",
+    "url",
+    "client_id",
+    "client_secret",
+    1.0
+  );
   beforeEach(() => {
     // creates a fresh pinia and make it active so it's automatically picked
     // up by any useStore() call without having to pass it to it:
     // `useStore(pinia)`
     setActivePinia(createPinia());
-    ospServiceStore = useOspServiceStore();
-    ospServiceStore.setServices([]);
+  });
+
+  afterEach(() => {
+    // cleanup the pinia instance
+    setActivePinia(undefined);
   });
 
   /// Test the getServices function
-  it("Should set the services", () => {
+  it("Should set the services on the store", () => {
+    const ospServiceStore = useOspServiceStore();
     expect(ospServiceStore.services).toStrictEqual([]);
+    let services = [testServiceModel, testServiceModel];
+    ospServiceStore.services = services;
+    expect(ospServiceStore.services.length).toBe(2);
+    expect(ospServiceStore.services[0]).toStrictEqual(testServiceModel);
   });
 
   /// Test add service
-  it("Should add a service", () => {
+  it("Should add a service on the store", () => {
+    const ospServiceStore = useOspServiceStore();
     expect(ospServiceStore.services).toStrictEqual([]);
+    ospServiceStore.addService(testServiceModel);
+    expect(ospServiceStore.services.length).toBe(1);
   });
 
   /// Test delete service
   it("Should delete a service", () => {
-    expect(ospServiceStore.services).toStrictEqual([]);
+    const ospServiceStore = useOspServiceStore();
+    let services = [testServiceModel];
+    ospServiceStore.services = services;
+    ospServiceStore.deleteService(testServiceModel._id);
+    expect(ospServiceStore.services.length).toBe(0);
   });
 
   /// Test update service
   it("Should update a service", () => {
-    expect(ospServiceStore.services).toStrictEqual([]);
+    const ospServiceStore = useOspServiceStore();
+    let services = [testServiceModel];
+    ospServiceStore.services = services;
+    let updatedService = testServiceModel;
+    updatedService.name = "updated name";
+    ospServiceStore.updateService(updatedService);
+    expect(ospServiceStore.services[0].name).toBe("updated name");
   });
 });
