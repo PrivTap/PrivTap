6a0325a0ac340cc559c024da803b5056c545e45b
Enna Basic <ebc21002@student.mdu.se>
2022-11-30 21:09:25 +0100

    ActionModel edited, methods finished,  tests passed [develop]

--- frontend/src/services/manage_action.ts
+++ frontend/src/services/manage_action.ts
@@ -3,38 +3,70 @@ import { http } from "@/helpers/axios_service";
 import type ActionModel from "@/model/action_model";
 import type { AxiosInstance } from "axios";
 import { ref } from "vue";
+import { useToast } from "vue-toastification";
  
 
 
-export default interface IManageAction {
-    getAllActions(): Promise<ActionModel[] | null>;
+export interface IManageAction {
+
+  createAction(
+    actionId: string,
+    name: string,
+    description: string,
+    serviceName: string,
+    serviceId: string,
+  ): Promise<ActionModel | null>;
+
+  getActionById(
+    actionId: string
+    ): Promise<ActionModel | null>;
+
+  updateAction(
+    actionId: string,
+    name: string,
+    description: string,
+    serviceName: string,
+    serviceId: string,
+    ): Promise<ActionModel | null>;
+
+  deleteAction(
+    actionId: string
+    ): Promise<ActionModel[] | null>;
+
+  getAllActions(
+
+    ): Promise<ActionModel[] | null>;
+
   }
   
-  export class ManageAction implements IManageAction {
+  export class ManageAction
+   implements IManageAction {
+  
 
     private static _instance: ManageAction;
-    http: AxiosInstance;
-  
+    
+    http: AxiosInstance; 
     private constructor() {
       this.http = http();
     }
 
   
-    public static getInstance(): ManageAction {
+     static get getInstance(): ManageAction {
       if (!ManageAction._instance) {
         ManageAction._instance = new ManageAction();
       }
       return ManageAction._instance;
     }
-  
+ 
+
     path: string = "/manage-actions";
   
-    rules = ref<ActionModel[]>([]); 
+    actions = ref<ActionModel[]>([]); 
   
     async getAllActions(): Promise<ActionModel[] | null> {
       try {
         const res = await this.http.get(this.path);
-        this.rules.value = res.data.data as ActionModel[];
+        this.actions.value = res.data.data as ActionModel[];
         return res.data.data;
       } catch (error) {
         axiosCatch(error);
@@ -42,4 +74,77 @@ export default interface IManageAction {
       }
     }
 
+
+    async getActionById(actionId: string): Promise<ActionModel | null> {
+      try {
+        const res = await this.http.get(this.path, { params: { actionId } });
+        return res.data.data as ActionModel;
+      } catch (error) {
+        axiosCatch(error);
+        return null;
+      }
+    }
+
+    async createAction(
+      actionId: string,
+      name: string,
+      description: string,
+      serviceName: string,
+      serviceId: string,
+    ): Promise<ActionModel | null> {
+      const body = {
+        actionId: actionId,
+        name: name,
+        description: description,
+        serviceName: serviceName,
+        serviceId: serviceId,
+      };
+      try {
+        const res = await this.http.post(this.path, body);
+        useToast().success("Action created");
+        return res.data.data as ActionModel;
+      } catch (error) {
+        axiosCatch(error);
+        return null;
+      }
+    }
+  
+    
+  async updateAction(
+      actionId: string,
+      name: string,
+      description: string,
+      serviceName: string,
+      serviceId: string,
+  ): Promise<ActionModel | null> {
+    try {
+      const body = {
+        actionId: actionId,
+        name: name,
+        description: description,
+        serviceName: serviceName,
+        serviceId: serviceId,
+      }
+      const res = await this.http.put(this.path, body);
+      useToast().success("Action updated");
+      return res.data.data as ActionModel;
+    } catch (error) {
+      axiosCatch(error);
+      return null;
+    }
+  }
+  
+
+  async deleteAction(actionId: string): Promise<Array<ActionModel> | null> {
+    try {
+      const body = { "actionId": actionId }
+      const res = await this.http.delete(this.path, { data: body });
+      this.actions.value = this.actions.value.filter((action) => action._id !== actionId);
+      useToast().success("Action deleted");
+      return this.actions.value;
+    } catch (error) {
+      axiosCatch(error);
+      return null;
+    }
+  }
 }
--- frontend/test/service_test/manage_action.test.ts
+++ frontend/test/service_test/manage_action.test.ts
@@ -1,11 +1,12 @@
-import { StubbedInstance, stubInterface } from "ts-sinon";
-import { beforeAll, describe, expect, test } from "vitest";
-import IManageAction from "../../src/services/manage_action";
+import { beforeAll, beforeEach, describe, expect, test } from "vitest";
+import { ManageAction } from "../../src/services/manage_action";
 import ActionModel from "../../src/model/action_model";
+import MockAdapter from "axios-mock-adapter";
 
-describe("Manage Action Tests", () => {
-  let manageActionStub: StubbedInstance<IManageAction>;
-  let actionModel: ActionModel = new ActionModel(
+describe("Manage Action Test", () => {
+  let mock: MockAdapter;
+  const _manageAction = ManageAction.getInstance;
+  let testActionModel: ActionModel = new ActionModel(
     "Test Action id",
     "Test Action name",
     "Test Description",
@@ -13,28 +14,61 @@ describe("Manage Action Tests", () => {
     "Test Service id",
   );
 
+
   beforeAll(() => {
-    manageActionStub = stubInterface<IManageAction>();
+    mock = new MockAdapter(_manageAction.http);
+  });
+
+  beforeEach(() => {
+    mock.reset();
   });
 
 
-  
-  test("Should return a list of actions", async () => {
-    manageActionStub.getAllActions.resolves(
-      [actionModel]
+  test("should return a the created action", async () => {
+    mock.onPost(_manageAction.path).reply(200, { data: testActionModel });
+    const res = await _manageAction.createAction(
+      testActionModel._id,
+      testActionModel.name,
+      testActionModel.description,
+      testActionModel.serviceName,
+      testActionModel.serviceId,
     );
-    const res = await manageActionStub.getAllActions();
-    expect(res).not.toBe(null);
-    expect(res?.length).toBe(1);
-    expect(res).toBeInstanceOf(Array<ActionModel>);
+    expect(res.name).toEqual(testActionModel.name);
   });
 
+  test("should return a list of actions", async () => {
+    mock.onGet(_manageAction.path).reply(200, { data: [testActionModel] });
+    const res = await _manageAction.getAllActions();
+    expect(res).toEqual([testActionModel]);
+  });
 
+  test("should return an action with the passed id", async () => {
+    mock.onGet(_manageAction.path, { params: { "actionId": testActionModel._id } }).reply(200, {
+      data: testActionModel
+    }
+    );
+    const res = await _manageAction.getActionById(testActionModel._id);
+    expect(res.name).toBe(testActionModel.name);
+  });
 
-
-
-
-
-
-
- });
+ 
+  test("should return a list of actions after deletion", async () => {
+    mock.onDelete(_manageAction.path).reply(200, { data: [] });
+    const res = await _manageAction.deleteAction("Test Action id");
+    expect(res).to.empty;
+  });
+ 
+  test("should return the updated action", async () => {
+    testActionModel.name = "Updated Action Name";
+    mock.onPut(_manageAction.path).reply(200, { data: testActionModel });
+    const res: ActionModel = await _manageAction.updateAction(
+      testActionModel._id,
+      "Updated ActionName",
+      testActionModel.description,
+      testActionModel.serviceId,
+      testActionModel.serviceId,
+    );
+    expect(res).not.toBe(null);
+    expect(res.name).toEqual("Updated Action Name");
+  });
+});
