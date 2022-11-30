f05ac852feb705660dfc86dc7fa6eae96f5c7da2
lorenzovaccarini <lorenzomaria.vaccarini@mail.polimi.it>
2022-11-30 20:33:57 +0100

    feat [PRIV-68][PRIV-69]: Write tests for /service endpoint

--- backend/test/routes/Services.test.ts
+++ backend/test/routes/Services.test.ts
@@ -3,10 +3,8 @@ import chaiHttp = require("chai-http");
 import { createSandbox, SinonStub } from "sinon";
 import sinonChai = require("sinon-chai");
 import app from "../../src/app";
-import Service from "../../src/model/Service";
-import Authentication from "../../src/helper/authentication";
-
-
+import Authentication, { AuthError } from "../../src/helper/authentication";
+import Service, { IService } from "../../src/model/Service";
 
 use(chaiHttp);
 use(sinonChai);
@@ -14,10 +12,10 @@ use(sinonChai);
 const sandbox = createSandbox();
 
 describe("/services endpoint", () => {
-    /*
     let requester: ChaiHttp.Agent;
     let checkJWTStub: SinonStub;
-    let findServicesStub: SinonStub;
+    let findAllStub: SinonStub;
+
     before(() => {
         requester = request(app.express).keepOpen();
     });
@@ -27,35 +25,45 @@ describe("/services endpoint", () => {
     });
 
     beforeEach(() => {
-        checkJWTStub = sandbox.stub(Authentication, "checkJWT");
-        checkJWTStub.returns("userId");
-        findServicesStub = sandbox.stub(Service, "findServices");
+        checkJWTStub = sandbox.stub(Authentication, "checkJWT").returns({
+            userId: "someUserId",
+            active: true
+        });
+        findAllStub = sandbox.stub(Service, "findAll").resolves([{}] as IService[]);
     });
 
     afterEach(() => {
         sandbox.restore();
     });
+
     describe("GET /", () => {
-        it("should call the query with the correct parameter and succeed", async () => {
-            findServicesStub.resolves([]);
-            let res = await requester.get("/services");
-            expect(res).to.have.status(200);
-            expect(findServicesStub).to.have.been.calledOnceWith(undefined, undefined);
-            res = await requester.get("/services").query({ items: 3 });
-            expect(res).to.have.status(200);
-            expect(findServicesStub).to.have.been.calledWith(3, undefined);
-            res = await requester.get("/services").query({ items: 3, page: 1 });
-            expect(res).to.have.status(200);
-            expect(findServicesStub).to.have.been.calledWith(3, 1);
+        it ("should fail if the user is not confirmed", async () => {
+            checkJWTStub.returns({
+                userId: "someUserId",
+                active: false
+            });
+            const res = await requester.get("/services");
+            expect(res).to.have.status(403);
         });
-        it("should fail when the query to database is not working", async () => {
-            findServicesStub.resolves(null);
-            const res = await requester.get("/services").query({ items: "ciao", page: 1 });
+
+        it ("should fail if the user doesn't have valid jwt", async () => {
+            checkJWTStub.throws(new AuthError());
+            const res = await requester.get("/services");
+            expect(res).to.have.status(401);
+        });
+
+        it ("should fail if an internal error occurs", async () => {
+            findAllStub.resolves(null);
+            const res = await requester.get("/services");
             expect(res).to.have.status(500);
+        });
+
+        it ("should succeed if the user is correctly authenticated and no internal error occurs", async () => {
+            const res = await requester.get("/services");
+            expect(res).to.have.status(200);
         });
     });
 
-     */
 });
 
 
