583fa65ce5bbdf6e974f305b4f7ff463ce38f334
Ivan Radonjic <118177516+chaser-22@users.noreply.github.com>
2022-12-20 00:46:42 +0100

    feat [PRIV-54]: Rule Visualization Page Finished

---
+++ frontend/src/components/RuleCard.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="bg-violet-900/20 rounded-lg py-8 px-8 shadow-lg">
+
+        <h3 class="text-2xl font-medium"> {{ rule.name }} </h3>
+
+        <v-label  class="pt-4 pb-2"> Trigger  </v-label>
+        <div>
+            <v-chip
+                class="mr-2">
+                {{rule.triggerId }}
+            </v-chip>
+        </div>
+
+            <v-label  class="pt-4 pb-2"> Action </v-label>
+
+
+
+         <div> 
+            <v-chip
+                class="mr-2">
+                {{rule.actionId }}
+            </v-chip>
+
+
+        </div>
+ 
+        <div class="flex justify-center">
+
+              <v-btn class="ma-2" color="error" size="small" variant="flat" @click="showModal = true">
+              <v-icon start icon="mdi-delete"></v-icon>
+              Delete
+              </v-btn>
+            </div>
+ 
+ 
+        <div class="flex justify-start mt-5 space-x-5">
+
+
+        </div>
+
+    </div>
+
+ 
+    <div v-if="showModal">
+    <ModalComponent title="Are you sure?" subTitle="You are deleting the rule permanently"
+      :onPressed="onModalClose" />
+  </div> 
+ 
+</template>
+
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue';
+import rules_controller from '@/controllers/rules_controller'; 
+import RoutingPath from "@/router/routing_path";
+import {useRouter} from "vue-router";
+import type RuleModel from '@/model/rule_model';
+import ModalComponent from './ModalComponent.vue';
+ 
+ 
+ 
+
+const showModal = ref(false);
+
+const router = useRouter();
+
+
+const props = defineProps<{
+    rule: RuleModel;
+
+}>();
+
+function onModalClose(res: boolean | null) {
+  showModal.value = false;
+  if (res) {
+    rules_controller.deleteRule(props.rule._id);
+  }
+}
+
+ 
+
+
+
+
+
+
+
+
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/PersonalPageView.vue
+++ frontend/src/views/PersonalPageView.vue
@@ -1,6 +1,48 @@
 <template>
  
 
+ <div class="h-full">
+
+<div class="flex flex-col items-center justify-center relative h-full">
+<img :src="radial" class="w-1/2">
+<div class="absolute top-2 w-full h-5/6">
+  <h1 class="text-5xl text-blue-100 text-center pb-12 font-medium">
+     My Rules
+  </h1>
+
+  <div v-if="!rules.length && !isLoading" class="flex flex-col justify-center items-center content-center ">
+    <h1 class="text-3xl text-blue-100 text-center pt-8 font-medium">
+     You have not created any rules yet
+    </h1>
+    <h1 class="text-lg text-stone-400 text-center pt-4 pb-10 font-medium">
+      Create a rule by clicking a button below
+    </h1>
+    <img :src=empty class="h-72 ">
+  </div>
+
+
+  <div v-if="isLoading" class="flex flex-col justify-center items-center content-center ">
+    <img :src="logo" class="h-32 my-20 animate-bounce">
+  </div>
+
+  <div class="flex flex-col justify-center items-center content-center">
+    <PrimaryButton :onClick="() => router.push(RoutingPath. CREATE_RULE_PAGE)" text="Create New Rule"
+      class="min-w-fit" />
+  </div>
+
+  <div v-if="rules?.length && !isLoading"  class="py-10">
+   <div class="flex justify-center items-center mt-20 gap-10">
+      <RuleCard v-for="(item, index) in rules" :key="index" :rule="item" />
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+</div>
+
 
 
 
