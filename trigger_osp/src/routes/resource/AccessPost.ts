76334908ba946d4d88018687f36d51c2d212f59b
Alessandro Sassi <AleSassi@users.noreply.github.com>
2022-12-20 18:51:21 +0100

    feat [PRIV-96]: fixes a crash in the dummy trigger OSP server due to a change in the query string format

--- trigger_osp/src/routes/resource/AccessPost.ts
+++ trigger_osp/src/routes/resource/AccessPost.ts
@@ -12,14 +12,7 @@ export default class AccessPostRoute extends Route {
         console.log("resource server called");
         const bearer = request.headers.authorization as string;
         const oauthToken = bearer.split(" ")[1];
-        const queryStr = request.query.authDetails as string;
-        let query = {};
-        try {
-            query = JSON.parse(queryStr);
-        } catch (error) {
-            console.log(error);
-        }
-        const permissions: {[id:string]:string}[] = []
+        const permissions = request.query.authDetails as {[id:string]:string}[];
         const authorization = await Authorization.findByToken(oauthToken);
         const actionDataFilter = request.query.filter; //TODO: Filter retrieved data with only what was requested by the action
 
@@ -29,8 +22,6 @@ export default class AccessPostRoute extends Route {
             return;
         }
         const userId = authorization.userId;
-
-        Object.values(query).forEach(value => permissions.push(value as {[id:string]:string}));
 
         if (!await Permission.authorized(userId, permissions)){
             response.status(401).send();
