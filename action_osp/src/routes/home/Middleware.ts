401ae6e66483d19f70edff0760ad97e4503e571d
Alessandro Sassi <AleSassi@users.noreply.github.com>
2022-12-28 22:23:05 +0100

    feat [PRIV-93]: implements the middleware architecture for the dummy action OSP, together with data format conversion from the PrivTAP standard and the action OSP one

---
+++ action_osp/src/routes/home/Middleware.ts
@@ -0,0 +1,39 @@
+import Route from "../../Route";
+import {Request, Response} from "express";
+import {ITriggerData, TriggerDataUtils} from "../../helper/dataDefinition";
+import {postReqHttp} from "../../helper/misc";
+
+export default class CreatePostRoute extends Route {
+    constructor() {
+        super("create-post", false);
+    }
+
+    protected async httpPost(request: Request, response: Response): Promise<void> {
+        const bearer = request.headers.authorization as string;
+        const dataFromTrigger = request.body as ITriggerData;
+        const convertedData = TriggerDataUtils.convertToInternalRepresentation(dataFromTrigger);
+
+        if (!convertedData) {
+            response.status(400);
+            return;
+        }
+
+        //Receive the data from the trigger internal resource server endpoint
+        let axiosResponse;
+        try {
+            axiosResponse = await postReqHttp(request.protocol + '://' + request.get("host") + "/create-post-internal", bearer, convertedData);
+            if (axiosResponse?.status != 200) {
+                console.log("error inserting post");
+                response.status(500).send();
+                return;
+            }
+        } catch (e) {
+            console.log("Axios response status =", axiosResponse?.status, "Error:", e);
+            response.status(500).send();
+            return;
+        }
+
+        response.status(200).send();
+    }
+
+}
